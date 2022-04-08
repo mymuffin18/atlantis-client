19b8ce649a8218455311ea7bb4a33a5ed6da9d72
Josh Uy <angatngheaven@gmail.com>
2022-04-08 16:42:35 +0800

    add immediate reports functionality

--- src/api/disaster_reports.js
+++ src/api/disaster_reports.js
@@ -17,14 +17,36 @@ export const addReport = async (data, token) => {
 			}
 		);
 		return_data = res.data;
-		console.log(res.data);
 	} catch (e) {
 		if (e.response.status === 401) {
 			alert('Please log in.');
 		}
-		console.log(e.response);
 		status = e.response.status;
 		errors = e.response.data;
+	}
+
+	return { return_data, status, errors };
+};
+
+export const postReport = async (data, token) => {
+	let status;
+	let errors = {};
+	let return_data = {};
+	try {
+		const res = await axios.post(
+			`${API_URL}/api/v1/disaster_reports`,
+			{ ...data },
+			{
+				headers: {
+					authorization: token,
+				},
+			}
+		);
+
+		return_data = res.data;
+	} catch (e) {
+		status = e.response.status;
+		errors = e.response.errors;
 	}
 
 	return { return_data, status, errors };
